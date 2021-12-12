7cda46362933cf566f08056de8d6860519720e53
Ubpa <ustczt@mail.ustc.edu.cn>
2021-12-12 15:50:59 +0800

    03.04 comment

---
+++ OfficialTutorial/03_common_programming_concepts/03_04_comment/project/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    // line comment
+    println!("add_one(3): {}", add_one(3) /* block comment */);
+}
+
+// vvv document comment
+
+/// Adds one to the number given.
+///
+/// # Examples
+///
+/// ```
+/// let arg = 5;
+/// let answer = my_crate::add_one(arg);
+///
+/// assert_eq!(6, answer);
+/// ```
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
